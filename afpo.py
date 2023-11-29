4d496882eaf62f7547a174d64d92ec4259f019d8
Nate Gaylinn <nate.gaylinn@gmail.com>
2023-11-29 11:54:56 -0500

    Code simplification / cleanup: Solution is more self contained, population is a list.

--- afpo.py
+++ afpo.py
@@ -7,28 +7,22 @@ import numpy as np
 from simulation import simulate, get_layer_mask, DEAD, ALIVE, WORLD_SIZE, NUM_STEPS, NUM_LAYERS, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS
 
 class Solution:
-    def __init__(self, id):
-        self.id = id
+    def __init__(self):
         self.age = 0
         self.been_simulated = False
         self.fitness = None
+        self.randomize_genome()
 
-        self.initialize_genome()
-
-    def set_id(self, new_id):
-        self.id = new_id
-    
-    def reset_age(self):
-        self.age = 0
-
-    def get_age(self):
-        return self.age
+    def make_offspring(self):
+        child = copy.deepcopy(self)
+        child.mutate()
+        child.age = 0
 
     def increment_age(self):
         self.age += 1
 
-    def set_simulated(self, new_simulated):
-        self.been_simulated = new_simulated
+    def set_simulated(self):
+        self.been_simulated = True
 
     def get_fitness(self):
         return self.fitness
@@ -36,10 +30,10 @@ class Solution:
     def mutate(self):
         pass
 
-    def dominates_other(self, other_solution):
-        return all([self.age < other_solution.get_age(), self.fitness < other_solution.get_fitness()])
+    def dominates(self, other):
+        return all([self.age < other.age, self.fitness < other.fitness])
 
-    def initialize_genome(self):
+    def randomize_genome(self):
         # Randomly initialize the NN weights (3 layers, input neurons, output neurons)
         self.genotype = np.random.random((3, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS)).astype(np.float32) * 2 - 1
 
@@ -47,27 +41,19 @@ class Solution:
 
 class AgeFitnessPareto:
     def __init__(self, experiment_constants):
-        self.current_generation = 0
-        self.population = {}
-
         self.max_generations = experiment_constants['max_generations']
         self.target_population_size = experiment_constants['target_population_size']
-
+        self.population = []
 
     def evolve(self):
-        # Initialize population if the current generation is 0
-        if self.current_generation == 0:
-            self.initialize_population()
-
-        # Evolve generations one at a time
-        while self.current_generation <= self.max_generations:
+        self.initialize_population()
+        for _ in range(self.max_generations):
             self.evolve_one_generation()
-            self.current_generation += 1
-
 
     def evolve_one_generation(self):
         # Increment ages by 1
-        self.increment_ages()
+        for sol in self.population:
+            sol.increment_age()
         # Extend the population using tournament selection
         self.extend_population()
 
@@ -89,91 +75,55 @@ class AgeFitnessPareto:
 
         # Make sure every individual has been set to simulated
         for sol in self.population:
-            sol.set_simulated(True)
+            sol.set_simulated()
 
-        # Reduce the population 
+        # Reduce the population
         self.reduce_population()
-
 
     def initialize_population(self):
         # Initialize target_population_size random solutions
-        for _ in range(self.target_population_size):
-            random_id = hash(np.random.random())
-            self.population[random_id] = Solution(random_id)
-
+        self.population = [
+            Solution() for _ in range(self.target_population_size)
+        ]
 
     def extend_population(self):
-        # 1 - Breed: do tournament selection 
+        # 1 - Breed: do tournament selection
         for _ in range(self.target_population_size):
             # Randomly select an individual using tournament selection
             parent = self.tournament_select()
+            self.population.append(parent.make_offspring())
 
-            # Child is a deepcopy of the parent 
-            child = copy.deepcopy(parent)
-
-            # Mutate the child
-            child.mutate()
-
-            # Add the child to the population
-            random_id = hash(np.random.random())
-            child.set_id(random_id)
-            child.reset_age()
-            self.population[random_id] = child
-
-        # Add a single random individual 
-        random_id = hash(np.random.random())
-        self.population[random_id] = Solution(random_id)
-
+        # Add a single random individual
+        self.population.append(Solution())
 
     def reduce_population(self):
         # Remove individuals until target population is reached
         while len(self.population) > self.target_population_size:
-            i1 = np.random.choice(list(self.population.keys()))
-            i2 = np.random.choice(list(self.population.keys()))
-            while i2 == i1:
-                i2 = np.random.choice(list(self.population.keys()))
-            if self.dominates(i1, i2): # i1 dominates
-                self.population.pop(i2)
-            elif self.dominates(i2, i1): # i2 dominates
-                self.population.pop(i1)
-
-    
-    def dominates(self, i1, i2):
-        return self.population[i1].dominates_other(self.population[i2])
-
+            sol1, sol2 = np.random.choice(self.population, 2, replace=False)
+            # Note that it's possible that NEITHER dominates the other.
+            if sol1.dominates(sol2):
+                self.population.remove(sol1)
+            elif sol2.dominates(sol1):
+                self.population.remove(sol2)
 
     def tournament_select(self):
-        """ 
-        Tournament selection randomly chooses two individuals from the population and 
+        """
+        Tournament selection randomly chooses two individuals from the population and
         selects the better (based on a primary objective) of the two for reproduction/mutation
         """
-        p1 = np.random.choice(list(self.population.keys()))
-        p2 = np.random.choice(list(self.population.keys()))
-        while p2 == p1:
-            p2 = np.random.choice(list(self.population.keys()))
-
-        # Tournament over the primary objective 
-        if self.population[p1].get_fitness() > self.population[p2].get_fitness():
-            return p1
-        else:
-            return p2
-
+        sol1, sol2 = np.random.choice(self.population, 2, replace=False)
+        return sol1 if sol1.get_fitness() > sol2.get_fitness() else sol2
 
     def evaluate_phenotypes(self, phenotypes):
         pass
 
-
     def get_unsimulated_genotypes(self):
-        # Filter out the unsimulated solutions
-        unsimulated_solutions = [self.population[id] for id in self.population if not sol.been_simulated]
-
+        # Filter out just the genotypes that haven't been simulated yet.
+        unsimulated_genotypes = [
+            sol.genotype for sol in self.population if not sol.been_simulated
+        ]
         # Aggregate the genotypes into a single matrix for simulation
-        genotypes = np.random.random((len(unsimulated_solutions), 3, NUM_INPUT_NEURONS, NUM_OUTPUT_NEURONS)).astype(np.float32) * 2 - 1
-        for i, sol in enumerate(unsimulated_solutions):
-            genotypes[i] = sol.genotype
-
-        return genotypes
-
+        return np.array(unsimulated_genotypes, dtype=np.float32)
 
     def make_seed_phenotypes(self):
         """Starting phenotypes to use by default (one ALIVE cell in middle)."""
@@ -194,10 +144,3 @@ class AgeFitnessPareto:
             phenotypes[i][0][0][middle][middle] = ALIVE
 
         return phenotypes
-
-
-    def increment_ages(self):
-        for individual in self.population:
-            individual.increment_age()
-
-    
