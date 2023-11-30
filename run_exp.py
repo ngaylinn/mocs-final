b0e5f05fe79e43b82fdc4e7ce7d409005109f40e
Kam Bielawski <kamtb28@gmail.com>
2023-11-29 19:23:46 -0500

    experiment setup

--- run_exp.py
+++ run_exp.py
@@ -1,4 +1,8 @@
 from pathlib import Path
+import tracemalloc
+import linecache
+import os
+import time
 
 import numpy as np
 
@@ -6,9 +10,10 @@ from afpo import AgeFitnessPareto
 from simulation import visualize
 
 experiment_parameters = {
-    'num_trials': 5,
+    'num_trials': 1,
     'target_population_size': 500,
     'max_generations': 2000,
+    'layers': 3
 }
 
 def main():
@@ -20,7 +25,7 @@ def main():
     # layers (hierarchical CA)
     # TODO: Do we also want to try with and without growth enabled?
     for arm in ('Control', 'Experiment'):
-        layers = 0 if arm == 'Control' else 2
+        layers = 1 if arm == 'Control' else 3
         fitness_scores = []
         # Run a few instances of each
         for trial in range(experiment_parameters['num_trials']):
@@ -31,10 +36,13 @@ def main():
             # only if necessary.
             single_run = AgeFitnessPareto(experiment_parameters)
             sol = single_run.evolve()
+
+            single_run.pickle_afpo(f'{arm}_t{trial}.pkl')
+
             fitness_scores.append(sol.fitness)
-            visualize(
-                sol.phenotype[0], # Save just the first layer
-                f'{arm}_t{trial}_f{sol.fitness}.gif')
+            # visualize(
+            #     sol.phenotype[0], # Save just the first layer
+            #     f'{arm}_t{trial}_f{sol.fitness}.gif')
 
         print(f'Mean fitness for {arm}: {np.mean(fitness_scores)}')
 
