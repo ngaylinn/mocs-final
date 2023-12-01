9aaada2f068bac6a850c558e2c68f49b432523c6
Kameron Bielawski <ktbielaw@vacc-user1.cluster>
2023-12-01 14:46:35 -0500

    experiment files

--- run_exp.py
+++ run_exp.py
@@ -3,46 +3,54 @@ import tracemalloc
 import linecache
 import os
 import time
+import argparse
 
 import numpy as np
 
 from afpo import AgeFitnessPareto
 from simulation import visualize
 
-experiment_parameters = {
-    'num_trials': 1,
-    'target_population_size': 500,
-    'max_generations': 2000,
-    'layers': 3
-}
+# Parse command line arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--exp_file', required=True, type=str, help='Experiment file')
+parser.add_argument('--name', required=True, type=str, help='Name of experiment')
+args = parser.parse_args()
+
+# Read the experiment file into exp_arms variable
+exp_file = open(args.exp_file)
+exp_string = exp_file.read()
+exp_arms = eval(exp_string)
+exp_file.close()
+
+# Create experiment directory if it doesn't exist
+if not os.path.exists('./experiments'):
+    os.system('mkdir experiments')
+if not os.path.exists(f'./experiments/{args.name}'):
+    os.system(f'mkdir ./experiments/{args.name}')
+
+# Copy experiment file into experiment directory
+os.system(f'cp {args.exp_file} ./experiments/{args.name}')
 
 def main():
-    # Delete all the visualizations generated in the last run (if any exist)
-    for file in Path().glob('*.gif'):
-        file.unlink()
+    for arm in exp_arms:
+        # Create an arm directory if it doesn't already exist
+        if not os.path.exists(f'./experiments/{args.name}/{arm}'):
+            os.system(f'mkdir ./experiments/{args.name}/{arm}')
 
-    # Try running with 0 extra layers (control, traditional CA) or with 2 extra
-    # layers (hierarchical CA)
-    # TODO: Do we also want to try with and without growth enabled?
-    for arm in ('Control', 'Experiment'):
-        layers = 1 if arm == 'Control' else 3
         fitness_scores = []
+        arm_parameters = exp_arms[arm]
         # Run a few instances of each
-        for trial in range(experiment_parameters['num_trials']):
-            experiment_parameters['layers'] = layers
+        for trial in range(arm_parameters['num_trials']):
             # TODO: It would be more efficient to run all trials for both
             # experiment and control in one batch, but that would also take
             # some non-trivial refactoring. We should add that optimization
             # only if necessary.
-            single_run = AgeFitnessPareto(experiment_parameters)
-            sol = single_run.evolve()
+            single_run = AgeFitnessPareto(arm_parameters)
+            best = single_run.evolve()
 
-            single_run.pickle_afpo(f'{arm}_t{trial}.pkl')
+            single_run.pickle_afpo(f'./experiments/{args.name}/{arm}/{arm}_t{trial}.pkl')
 
-            fitness_scores.append(sol.fitness)
-            # visualize(
-            #     sol.phenotype[0], # Save just the first layer
-            #     f'{arm}_t{trial}_f{sol.fitness}.gif')
+            fitness_scores.append(best.fitness)
 
         print(f'Mean fitness for {arm}: {np.mean(fitness_scores)}')
 
