62ca7d349b3f3b29ce5462e900252e0176e6c1ce
Kameron Bielawski <ktbielaw@vacc-user1.cluster>
2025-03-22 17:54:00 -0400

    activate hnca-cuda environment

--- vacc_submit_arm.sh
+++ vacc_submit_arm.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --gpus=1
-#SBATCH --job-name=mocs_exp_%j
+#SBATCH --job-name=hnca_%j
 #SBATCH --output=./vacc_out_files/%x_%j.out
 #SBATCH --time=9:00:00
 
@@ -14,5 +14,7 @@ echo $1
 echo $2
 echo $3
 echo $4
+
+conda activate hnca-cuda
 
 python3 -O run_trial.py "$1" "$2" "$3" "$4"
